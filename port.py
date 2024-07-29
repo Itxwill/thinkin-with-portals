85208b01c5a7a12c085dfa6ff983416d138968b9
Itxwill <williamheggroll@gmail.com>
2024-07-29 16:12:28 -0700

    this is the first update, mainly just setting up

--- port.py
+++ port.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame as pyg
 
 pyg.init()
@@ -5,19 +6,74 @@ width, height = 1200, 800
 screen = pyg.display.set_mode((width, height))
 clock = pyg.time.Clock()
 
-playerpos = [600,400]
-playervel = [0,0]
-playersize = 15
-objects = []
+# Constants
+playerPos = [600,400]
+playerVel = [0,0]
+playerSize = 15
+
+gravityConstant = .1
+gameMap = []
+debugObjs = []
+
+# Handler functions
+def dist(p1,p2):
+    return ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**(1/2)
 
 
+# Classes
+class Line():
+    def __init__(self,p1,p2,width=1,color=(255,255,255)):
+        self.p1=p1
+        self.p2=p2
+        self.color=color
+        self.width=width
+    def draw(self):
+        pyg.draw.line(screen,self.color,self.p1,self.p2,self.width)
 
-def drawplayer():
-    playervel[1]+=.1
+class Wall():
+    def __init__(self,left,top,width,height,lineWidth=5,color=(255,255,255)):
+        self.color=color
+        self.left = left
+        self.top=top
+        self.width=width
+        self.height=height
+        self.lineWidth=lineWidth
 
-    playerpos[0]+=playervel[0]
-    playerpos[1]+=playervel[1]
-    pyg.draw.circle(screen,(255,255,255), playerpos, playersize,1)
+        self.center = (left+width/2,top+height/2)
+
+    def draw(self):
+        pyg.draw.rect(screen,self.color,pyg.Rect(self.left,self.top,self.width,self.height),width=self.lineWidth)
+
+
+testWall = Wall(0,750,1200,50)
+gameMap.append(testWall)
+
+# Secondary functions
+def drawMap():
+    for block in gameMap:
+        block.draw()
+
+def drawDebug():
+    for deco in debugObjs:
+        deco.draw()
+
+def drawPlayer():
+    global playerPos,playerVel,playerSize
+
+    # Handles gravity
+    playerVel[1]+=gravityConstant
+
+    # Handles velocity
+    playerPos[0]+=playerVel[0]
+    playerPos[1]+=playerVel[1]
+
+    for block in gameMap:
+        closestPoint = (max(0,min(playerPos[0]-block.left,block.width)), block.top-playerPos[1])
+        pyg.draw.circle(screen, (255,0,0), closestPoint, 5,width=0)
+        #if dist(closestPoint,playerPos)<playerSize:
+            
+
+    pyg.draw.circle(screen, (255,255,255), playerPos, playerSize,width=1)
     
 
 while True:
@@ -25,6 +81,10 @@ while True:
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
             exit()
+
+    drawPlayer()
+    drawMap()
+    drawDebug()
+
     clock.tick(120)
-    drawplayer()
     pyg.display.update()
